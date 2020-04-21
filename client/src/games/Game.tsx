0d9d7c01e78dadf95736aa1299a9a5bfdff1f4ad
Jacqueline Askins <askinsjacqueline@gmail.com>
2020-04-21 18:49:12 -0400

    Add error handling on board validation

--- client/src/games/Game.tsx
+++ client/src/games/Game.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, Typography } from '@material-ui/core';
 import Backend from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
+import { useSnackbar } from 'notistack';
 
 import Board from '../boards/Board';
 import Button from '../buttons/Button';
@@ -15,6 +16,7 @@ import { isConnectedBoard } from '../boards/validate';
 type GameProps = {};
 
 const Game: React.FC<GameProps> = () => {
+  const { enqueueSnackbar } = useSnackbar();
   const { socket } = useSocket();
   const {
     gameInfo: { bunchSize, players },
@@ -23,6 +25,15 @@ const Game: React.FC<GameProps> = () => {
   const { board, hand } = players.find(
     (player) => player.userId === socket.id
   ) as Player;
+
+  let disabled;
+  try {
+    disabled = Object.values(hand).length > 0 || !isConnectedBoard(board);
+  } catch (err) {
+    enqueueSnackbar('There was an error validating the board');
+    console.log(err);
+    disabled = false;
+  }
 
   return (
     <DndProvider backend={Backend}>
@@ -48,9 +59,7 @@ const Game: React.FC<GameProps> = () => {
                 onClick={(): void => {
                   socket.emit('peel', {});
                 }}
-                disabled={
-                  Object.values(hand).length > 0 || !isConnectedBoard(board)
-                }
+                disabled={disabled}
               >
                 {bunchSize < players.length ? 'Bananas!' : 'Peel!'}
               </Button>
