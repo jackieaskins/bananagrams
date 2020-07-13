eb3d4d249b695b2b4b1b7846e0b72ce510461b91
Jacqueline Askins <askinsjacqueline@gmail.com>
2020-07-13 19:03:47 -0400

    Move words to typescript file

--- server/src/dictionary/Dictionary.ts
+++ server/src/dictionary/Dictionary.ts
@@ -1,16 +1,11 @@
-import fs from 'fs';
-import path from 'path';
-
 import Trie from './Trie';
+import words from './words';
 
 export default class Dictionary {
   static trie: Trie;
 
   static initialize(): void {
     this.trie = new Trie();
-
-    const file = path.resolve(path.join(__dirname, './words.txt'));
-    const words = fs.readFileSync(file, 'utf-8').split('\n');
 
     words.forEach((word) => {
       this.trie.insert(word.trim());
