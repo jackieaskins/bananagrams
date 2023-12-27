362f432a92f34de847696bd73bcffd0c8ce86ea2
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-12-27 17:27:51 -0500

    Add eslint-config workspace

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -1,7 +1,5 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:jest/recommended",
     "plugin:jest/style",
     "plugin:jest-dom/recommended",
@@ -10,53 +8,21 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
     "plugin:testing-library/react",
-    "prettier",
+    "bananagrams",
   ],
-  overrides: [
-    {
-      files: ["*.test.ts", "*.test.tsx"],
-      rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaFeatures: { jsx: true },
   },
-  plugins: ["import", "react-refresh"],
-  reportUnusedDisableDirectives: true,
+  plugins: ["react-refresh"],
   rules: {
-    "@typescript-eslint/explicit-module-boundary-types": "error",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "arrow-body-style": ["error", "as-needed"],
-    "import/order": [
-      "error",
-      {
-        alphabetize: { order: "asc" },
-        groups: [["builtin", "external"], "parent", "sibling", "index"],
-        "newlines-between": "never",
-      },
-    ],
     "jest/expect-expect": [
       "error",
       { assertFunctionNames: ["expect", "assert*"] },
     ],
-    "no-debugger": "error",
-    "no-useless-rename": "error",
-    "object-shorthand": "error",
     "react/self-closing-comp": "error",
     "react-refresh/only-export-components": "error",
-    "require-await": "error",
   },
   settings: {
-    react: {
-      version: "detect",
-    },
+    react: { version: "detect" },
   },
 };
---
+++ eslint-config/index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["import"],
+  reportUnusedDisableDirectives: true,
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "arrow-body-style": ["error", "as-needed"],
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc" },
+        groups: [["builtin", "external"], "parent", "sibling", "index"],
+        "newlines-between": "never",
+      },
+    ],
+    "no-debugger": "error",
+    "no-useless-rename": "error",
+    "object-shorthand": "error",
+    "require-await": "error",
+  },
+};
