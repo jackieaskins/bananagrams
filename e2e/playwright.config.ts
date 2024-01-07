63b4aeaa892cb0e10d53cd3959cbf94fa4cb3ce7
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-01-07 13:40:06 -0500

    Playwright: Build package before running npm start

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: `npm start`,
+    command: `npm run build && npm start`,
     cwd: "..",
     port: PORT,
     reuseExistingServer: !process.env.CI,
