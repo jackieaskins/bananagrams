7d6847ed61d106735bd996ca1fee0a627573995b
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-01-26 18:46:27 -0500

    feat(server): add server stats route

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Server } from "socket.io";
 import ViteExpress from "vite-express";
+import GameController from "./controllers/GameController";
 import Dictionary from "./dictionary/Dictionary";
 import { configureSocket } from "./socket";
 
@@ -8,6 +9,12 @@ const PORT = Number.parseInt(process.env.PORT || "3000");
 Dictionary.initialize();
 
 const app = express();
+
+app.get("/server/stats", (_, res) =>
+  res.send({
+    gameCount: Object.keys(GameController.getGames()).length,
+  }),
+);
 
 const server = ViteExpress.listen(app, PORT, () => {
   console.log(`Server has started on port ${PORT}`);
